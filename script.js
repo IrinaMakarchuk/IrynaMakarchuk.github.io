6c00553ba2ff1fec7ddb3a52bade685d1d103239
Irene Boboshko <boboshko@cyberiansoft.com>
2019-10-29 12:37:49 +0200

    task 3 updated

---
+++ script.js
@@ -0,0 +1,11 @@
+const navigation = document.getElementById('navigation-block')
+const burger = document.getElementById('burger-id')
+const closeMenu = document.getElementById('close-menu')
+
+burger.addEventListener('click', () => {
+  navigation.setAttribute('style', 'display: block')
+})
+
+closeMenu.addEventListener('click', () => {
+  navigation.setAttribute('style', 'display: none')
+})
